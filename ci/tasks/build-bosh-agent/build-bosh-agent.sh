d0ef0bfee360ba40ee92f41d5048f86ed0936783
Daniel Lavoie <dlavoie@live.ca>
2018-01-20 23:46:12 -0500

    WIP

--- ci/tasks/build-bosh-agent/build-bosh-agent.sh
+++ ci/tasks/build-bosh-agent/build-bosh-agent.sh
@@ -5,10 +5,6 @@ go get github.com/golang/lint/golint
 
 $GOPATH/src/github.com/cloudfoundry/bosh-agent/bin/build-linux-amd64
 
-mkdir -p $GOPATH/src/github.com/cloudfoundry/bosh-agent/out/release/usr/bin
-
-mv $GOPATH/src/github.com/cloudfoundry/bosh-agent/out/bosh-agent $GOPATH/src/github.com/cloudfoundry/bosh-agent/out/release/usr/bin/bosh-agent
-
-tar -czvf artifact-stemcell/scaleway-ubuntu-trusty-stemcell.tgz --directory=$GOPATH/src/github.com/cloudfoundry/bosh-agent/out/release/ .
+tar -czvf artifact-stemcell/scaleway-ubuntu-trusty-stemcell.tgz --directory=$GOPATH/src/github.com/cloudfoundry/bosh-agent/out/ /usr/bin
 
 ls artifact-stemcell/
